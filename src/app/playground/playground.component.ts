24384f12ab22aa5b8f08dcc2a435743df47788c9
Vivekanand P V <vivekanandpv@outlook.com>
2019-11-20 19:54:25 +0530

    finished

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -1,17 +1,38 @@
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  AfterViewInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-playground',
   templateUrl: './playground.component.html',
   styleUrls: ['./playground.component.css']
 })
-export class PlaygroundComponent implements OnInit {
+export class PlaygroundComponent implements OnInit, OnChanges, AfterViewInit {
   @Output() register = new EventEmitter();
   @Input() nClicks: number;
 
-  constructor() {}
+  constructor() {
+    console.log('Playground constructor is called');
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log('Playground ngOnInit is called');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Playground ngOnChanges is called');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('Playground ngAfterViewInit is called');
+  }
 
   clickHandler() {
     console.log('Click handled in playground component');
